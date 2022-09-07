f30475e0b07ae0a3bc7e713d4fb8f0729e358550
XShaan <shayan.sakha@gmail.com>
2022-09-08 01:41:46 +0430

    fix some issue

--- server.py
+++ server.py
@@ -3,20 +3,20 @@ import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/59b90c48a6e56a515635").json()
+
 
 @app.route("/")
 def home():
-    post_url = "https://api.npoint.io/59b90c48a6e56a515635"
-    response = requests.get(post_url)
-    all_posts = response.json()
-    return render_template("index.html", posts=all_posts)
+    return render_template("index.html", all_posts=posts)
 
 @app.route("/single/<int:id>")
 def get_single(id):
-    post_url = "https://api.npoint.io/59b90c48a6e56a515635"
-    response = requests.get(post_url)
-    single_post = response.json()
-    return render_template("single.html", post=single_post[id -1])
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == id:
+            requested_post = blog_post
+    return render_template("single.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
